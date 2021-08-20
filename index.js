a0214f2adcc866810641c30e3c95b9f671d01eb4
Benjamin <ben.d.maryland@gmail.com>
2021-08-20 15:56:07 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+    return string.toUpperCase(shout);
+  } 
+  function whisper(string) {
+    return string.toLowerCase(whisper) 
+  }
+  function logShout(string){
+      console.log(string.toUpperCase('hello'))
+  }
+
+
+  function logWhisper(string){
+    console.log(string.toLowerCase('hello'))
+}
+function sayHiToGrandma(string){
+if (string==string.toLowerCase()) {
+    return ("I can't hear you!")
+}
+else if(string==string.toUpperCase()) {
+    return ("YES INDEED!")    
+} 
+else 
+return ("I love you, too.")
+}
+
